6ef5be43c746a664b98733b352f89412a343d1f3
dmitr <d.privalov@softline.com>
2021-11-04 18:03:08 +0300

    Feat1

--- Start.java
+++ Start.java
@@ -1,11 +1,13 @@
 public class Start{
 	// Дополнение к комиту
-	//
+	// 
 	// 
 	public static void main(String... args){
 
 		System.out.println("Hello!!!!!");
 		System.out.println(HELLO_WORLLD);
 	}
+
+	public static void foo(){}
 
 }
