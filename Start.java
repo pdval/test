a727d2420695d1d25ec57cf9702301347cdf946f
dmitr <d.privalov@softline.com>
2021-11-04 15:50:32 +0300

    semicommit + 1

--- Start.java
+++ Start.java
@@ -1,11 +1,11 @@
 public class Start{
 	// Дополнение к комиту
+	//
+	// 
 	public static void main(String... args){
 
 		System.out.println("Hello!!!!!");
 		System.out.println(HELLO_WORLLD);
 	}
 
-	public static void doAny(){
-	}
 }
