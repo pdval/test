e6fefb08fad70e2c85a7220f5e409e2a746841a9
dmitr <d.privalov@softline.com>
2021-11-04 14:50:05 +0300

    Third commit java

--- Start.java
+++ Start.java
@@ -1,5 +1,7 @@
 public class Start{
+	public static String HELLO_WORLD = "Hello, world!";
+
 	public static void main(String... args){
-		System.out.println("Hello, world!");
+		System.out.println(HELLO_WORLLD);
 	}
 }
